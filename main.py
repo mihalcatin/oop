4ea1cbd9f54d6b1e1429d3b753d085d4ef1f1fe9
PY-TU-10-23 <mihalcatin@gmail.com>
2024-01-16 18:20:44 +0100

    oop

---
+++ main.py
@@ -0,0 +1,12 @@
+class MojaKalkulacka:
+    @staticmethod
+    def sucet (a, b):
+        return a + b
+
+    @staticmethod
+    def sucin(a, b):
+        return a*b
+
+
+print(MojaKalkulacka.sucet(2, 2))
+print(MojaKalkulacka.sucin(2, 3))
